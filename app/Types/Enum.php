7af402cd8482bd19379f6e6f3b625b393af5d244
Saeid Asadi <s.asadi@sarmadict.ir>
2018-02-18 23:52:40 +0330

    Add Enum service

---
+++ app/Types/Enum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Types;
+
+
+use MyCLabs\Enum\Enum as BaseEnum;
+
+class Enum extends BaseEnum
+{
+    //
+}
---
+++ app/Types/State.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Types;
+
+class State extends Enum
+{
+    const DISABLED = 1;
+
+    const ENABLED = 2;
+}
