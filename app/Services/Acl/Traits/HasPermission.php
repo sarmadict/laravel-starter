26f88d4437373391936311e28215b291f5bd1785
Saeid Asadi <s.asadi@sarmadict.ir>
2018-04-13 18:46:13 +0430

    store loaded permissions after first request to preven over requesting cache during one process Known Issues:  - loaded permissions should be saved in a singleton class to prevent (permissions) data loss after Model destroy

--- app/Services/Acl/Traits/HasPermission.php
+++ app/Services/Acl/Traits/HasPermission.php
@@ -10,6 +10,20 @@ use Illuminate\Support\Collection;
 trait HasPermission
 {
     /**
+     * Store permissions to prevent over requesting to cache store
+     *
+     * @var Collection
+     */
+    protected $loadedPermissions;
+
+    /**
+     * Determine if permissions are loaded or not
+     *
+     * @var bool
+     */
+    protected $arePermissionsLoaded = false;
+
+    /**
      * Get all the associated permissions
      *
      * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
@@ -26,12 +40,28 @@ trait HasPermission
      */
     public function getPermissions(): Collection
     {
+        if (!$this->arePermissionsLoaded) {
+            $this->loadedPermissions = $this->loadPermissions();
+
+            $this->arePermissionsLoaded = true;
+        }
+
+        return $this->loadedPermissions;
+    }
+
+    /**
+     * Load permissions from cache store
+     *
+     * @return mixed
+     */
+    public function loadPermissions(): Collection
+    {
         $cacheKey = property_exists($this, 'aclCacheKey') ? $this->aclCacheKey : $this->getTable();
 
         $config = config('acl.cache.' . $cacheKey);
 
         return cache()->remember($config['prefix'] . '.' . $this->getKey(), $config['expiration_time'], function () {
-            return $this->permissions->pluck('name');
+            return $this->permissions()->pluck('name');
         });
     }
 }
