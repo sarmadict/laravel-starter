aa005e116700c99535df2aa94c6a4ba3b4b2d109
Saeid Asadi <saeidasadi1995@gmail.com>
2018-09-15 03:25:38 +0430

    fix unauthenticated fallback routename

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -63,6 +63,6 @@ class Handler extends ExceptionHandler
     {
         return $request->expectsJson()
             ? response()->json(['message' => $exception->getMessage()], 401)
-            : redirect()->guest(route('accounts.auth.login.show'));
+            : redirect()->guest(route('auth.login.show'));
     }
 }
